aa812bfd65b722c2e37af1236380f1b835eedf3c
Dennis Nesterovsky <deanfit@gmail.com>
2017-03-23 10:35:18 +0200

    First commit

--- js/main.js
+++ js/main.js
@@ -7,5 +7,4 @@ $('body').on('click', '.burger-container', function(e){
 		e.preventDefault();
 			$(this).find('#burger').toggleClass('open');
 			$('.js').toggleClass('open');
-			// $('.js-right-block').toggleClass('open');
 	});
